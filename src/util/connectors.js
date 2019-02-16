f67aa4e642f75c8ae08a0822345701cdc4f6a6b7
Gareth Oates <goatie@gmail.com>
2019-02-15 20:01:14 -0700

    Added code for reference

--- src/util/connectors.js
+++ src/util/connectors.js
@@ -4,6 +4,15 @@ export let uport = new Connect('UPort Test App', {
   network: "rinkeby"
 })
 
+// const abi = [{"constant": false,"inputs": [{"name": "status","type": "string"}],"name": "updateStatus","outputs": [],"type": "function"}]
+// const contractAddress = '0x71845bbfe5ddfdb919e780febfff5eda62a30fdc'
+// const statusContract = uport.contract(abi).at(contractAddress)
+
+// statusContract.updateStatus('hello', 'updateStatusReq')
+// uport.onResponse('updateStatusReq').then(payload => {
+//   const txId = payload.res
+// })
+
 export const web3 = uport.getWeb3()
 
 window.uport = uport;
